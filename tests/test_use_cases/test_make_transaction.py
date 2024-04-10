d83bbac0df3dda3eea8de8f9fde183c43ebad755
G <jrggementiza@gmail.com>
2024-04-10 19:59:22 +0800

    Add persistance in make transaction use case

--- tests/test_use_cases/test_make_transaction.py
+++ tests/test_use_cases/test_make_transaction.py
@@ -5,6 +5,12 @@ import pytest
 from entities.account import Account
 from entities.transaction import Transaction
 from use_cases.make_transaction import MakeTransactionUseCase
+from repositories.transaction_repository import TransactionRepository
+
+
+@pytest.fixture
+def transaction_repository():
+    return TransactionRepository()
 
 
 @pytest.fixture
@@ -12,25 +18,34 @@ def new_account():
     return Account(1, 1, "TEST1234")
 
 
-def test_make_transaction(new_account):
-    make_transaction_use_case = MakeTransactionUseCase()  # TODO: Add repository and propagation
+def test_make_transaction(new_account, transaction_repository):
+    make_transaction_use_case = MakeTransactionUseCase(transaction_repository)
 
-    deposit_1 = Decimal('3.00')
-    transaction = make_transaction_use_case.make_transaction(new_account, "Deposit", deposit_1)
+    deposit_1 = Decimal("3.00")
+    transaction_1 = make_transaction_use_case.make_transaction(
+        new_account, "Deposit", deposit_1
+    )
 
-    assert isinstance(transaction, Transaction)
-    assert transaction.type == "Deposit"
+    assert isinstance(transaction_1, Transaction)
+    assert transaction_1.transaction_id == 1
+    assert transaction_1.type == "Deposit"
     assert new_account.get_balance() == deposit_1
 
-    withdraw_1 = Decimal('1.00')
-    transaction = make_transaction_use_case.make_transaction(new_account, "Withdraw", withdraw_1)
+    withdraw_1 = Decimal("1.00")
+    transaction_2 = make_transaction_use_case.make_transaction(
+        new_account, "Withdraw", withdraw_1
+    )
 
-    assert isinstance(transaction, Transaction)
-    assert transaction.type == "Withdraw"
-    assert new_account.get_balance() == Decimal('2.00')
+    assert isinstance(transaction_2, Transaction)
+    assert transaction_2.transaction_id == 2
+    assert transaction_2.type == "Withdraw"
+    assert new_account.get_balance() == Decimal("2.00")
 
-    withdraw_2 = Decimal('3.00')
+    withdraw_2 = Decimal("3.00")
     with pytest.raises(ValueError):
-            make_transaction_use_case.make_transaction(new_account, "Withdraw", withdraw_2)
+        make_transaction_use_case.make_transaction(new_account, "Withdraw", withdraw_2)
 
-    assert new_account.get_balance() == Decimal('2.00')
+    assert new_account.get_balance() == Decimal("2.00")
+
+    assert transaction_1.transaction_id in transaction_repository.storage
+    assert transaction_2.transaction_id in transaction_repository.storage
--- use_cases/make_transaction.py
+++ use_cases/make_transaction.py
@@ -7,6 +7,9 @@ class TransactionType(Enum):
 
 
 class MakeTransactionUseCase:
+    def __init__(self, repository):
+        self.transaction_repository = repository
+
     def make_transaction(self, account, transaction_type, amount):
         account_action_map = {
             TransactionType.DEPOSIT.value: "deposit",
@@ -15,7 +18,6 @@ class MakeTransactionUseCase:
 
         action = account_action_map[transaction_type]
         transaction = getattr(account, action)(amount)
+        saved_transaction = self.transaction_repository.create(transaction)
 
-        # TODO: Add propagation
-
-        return transaction
+        return saved_transaction
