7de9425dc58411ab2cc1d6387161554ee2a7a0b9
G <jrggementiza@gmail.com>
2024-04-10 19:40:58 +0800

    Add persistence in create customer use case

--- tests/test_use_cases/test_create_customer.py
+++ tests/test_use_cases/test_create_customer.py
@@ -1,15 +1,25 @@
+import pytest
+
 from entities.customer import Customer
 from use_cases.create_customer import CreateCustomerUseCase
+from repositories.customer_repository import CustomerRepository
 
 
-def test_create_customer():
-    create_customer_use_case = CreateCustomerUseCase()
-    new_customer = create_customer_use_case.create_customer("jr", "jr@yopmail.com", "+639171234567")
+@pytest.fixture
+def customer_repository():
+    return CustomerRepository()
+
+
+def test_create_customer(customer_repository):
+    create_customer_use_case = CreateCustomerUseCase(customer_repository)
+    new_customer = create_customer_use_case.create_customer(
+        "jr", "jr@yopmail.com", "+639171234567"
+    )
 
     assert isinstance(new_customer, Customer)
+    assert new_customer.customer_id == 1
     assert new_customer.name == "jr"
     assert new_customer.email == "jr@yopmail.com"
     assert new_customer.phone_number == "+639171234567"
 
-# TODO: Propagation
-# TODO: Test auto increment of customer_id
+    assert new_customer.customer_id in customer_repository.storage
--- use_cases/create_customer.py
+++ use_cases/create_customer.py
@@ -1,8 +1,12 @@
 from entities.customer import Customer
+from repositories.customer_repository import CustomerRepository
 
 
 class CreateCustomerUseCase:
+    def __init__(self, repository):
+        self.customer_repository = repository
+
     def create_customer(self, name, email, phone_number):
-        # TODO: Add propagation
-        # TODO: Add auto increment of customer_id
-        return Customer(name, email, phone_number)
+        customer = Customer(name, email, phone_number)
+        saved_customer = self.customer_repository.create(customer)
+        return saved_customer
